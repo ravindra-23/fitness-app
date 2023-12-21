1e7d58eaf1be3328a6153e8ce04b44070a2bc2df
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-12-21 15:36:23 +0530

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import Navbar from "@/components/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className="app bg-gray-20">
-      <h1 className="text-gray500 font-dmsans">Hello</h1>
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
