11d2d44fe1000025354afd7112871fffb56ffdff
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-12-22 14:47:41 +0530

    add Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Home from "@/components/home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -26,6 +27,8 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
